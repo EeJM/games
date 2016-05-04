5b1913abd577761593f1a9d53bbe8fe1810af413
Nikziful <Niko.holopainen.97@gmail.com>
2016-05-04 16:27:36 +0300

    Highscore

---
+++ src/Highscore/Highscore.java
@@ -0,0 +1,30 @@
+package Highscore;
+
+import javax.swing.*;
+
+
+public class Highscore extends JFrame{
+    
+    private JPanel basePanel=new JPanel();
+    
+    private JLabel name1=new JLabel();
+    private JLabel name2=new JLabel();
+    private JLabel name3=new JLabel();
+    private JLabel name4=new JLabel();
+    private JLabel name5=new JLabel();
+    private JLabel points1=new JLabel();
+    private JLabel points2=new JLabel();
+    private JLabel points3=new JLabel();
+    private JLabel points4=new JLabel();
+    private JLabel points5=new JLabel();
+    
+    private JButton menuButton=new JButton();
+
+    
+    public Highscore() {
+        
+    }
+    
+    
+    
+}
